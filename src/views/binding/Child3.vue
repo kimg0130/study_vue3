3cf78a28ec689d4e4f7e6c8a5ee60b07b2999cac
김지연 <kimg0130@gmail.com>
2023-04-21 14:28:47 +0900

    data binding test

---
+++ src/views/binding/Child3.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="pa-2" variant="outlined">
+    <div class="text-h6">child 3</div>
+    <v-text-field
+      v-model="name"
+      @input="inputChange($event, 'name')"
+      label="name"
+      required
+    ></v-text-field>
+
+<!--    binding시 자동으로 number형 변환-->
+    <v-text-field
+      v-model.number="age"
+      @input="inputChange($event, 'age')"
+      label="age"
+      required
+    ></v-text-field>
+  </v-card>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props = defineProps({
+  name: String,
+  age: Number
+})
+
+const name = ref(props.name)
+const age = ref(props.age)
+
+const emit = defineEmits(["update:name", "update:age"])
+const inputChange = (event, target) =>{
+  if(target === 'name')
+    emit('update:name', event.target.value);
+  else
+    emit('update:age', event.target.value);
+}
+
+</script>
--- src/views/binding/DataBinding.vue
+++ src/views/binding/DataBinding.vue
@@ -1,35 +1,54 @@
 <template>
 
-    <v-card
-      class="mx-auto"
-      variant="tonal"
-    >
-      <v-card-item>
-        <div>
-          <div class="text-h6 mb-1">
-            Data 바인딩
-          </div>
-          <div class="text-caption">
-            Vue3에서 양방향 바인딩을 위해서는 props 대신 v-model을 사용한다!<br/>
-            props로 넘긴데이터는 단방향임!
-          </div>
+  <v-card
+    class="mx-auto"
+    variant="tonal"
+  >
+    <v-card-item>
+      <div>
+        <div class="text-h6 mb-1">
+          Data 바인딩
         </div>
-      </v-card-item>
-    </v-card>
-    <div class="mt-10 text-h6">parent</div>
-    <div class="mt-5">[양방향] v-model value1 : {{value1}}</div>
-    <child-1 v-model="value1"/>
-    <div class="mt-5">[단방향] props value2 : {{value2}}</div>
-    <child-2 :text="value2" @updateChild="updateChild"/>
+        <div class="text-caption">
+          Vue3에서 양방향 바인딩을 위해서는 props 대신 v-model을 사용한다!<br/>
+          props로 넘긴데이터는 단방향임!
+        </div>
+      </div>
+    </v-card-item>
+  </v-card>
+
+  <v-divider class="mt-5 mb-5"></v-divider>
+
+  <div class="text-blue">[양방향] v-model</div>
+  <div class="mt-1">  value1 : {{value1}}</div>
+  <Child1 v-model="value1"/>
+
+  <v-divider class="mt-5 mb-5"></v-divider>
+
+  <div class="text-blue">[단방향] props</div>
+  <div class="mt-1"> value2 : {{value2}}</div>
+  <Child2 :text="value2" @updateChild="updateChild"/>
+
+  <v-divider class="mt-5 mb-5"></v-divider>
+
+  <div class="text-blue">v-model 여러개 지정 가능</div>
+  <div class="mt-1">name : {{name}}</div>
+  <div class="mt-1">age : {{age}}</div>
+  <Child3 v-model:name="name" v-model:age="age"/>
 
 </template>
 
 <script setup>
 import Child1 from "@/views/binding/Child1.vue";
 import Child2 from "@/views/binding/Child2.vue";
+import Child3 from "@/views/binding/Child3.vue";
+
 import {ref} from "vue";
 const value1 = ref('')
 const value2 = ref('기본 값')
+
+const name = ref('jiyeon')
+const age = ref(41)
 
 const updateChild = (data) => {
   console.log(data);
