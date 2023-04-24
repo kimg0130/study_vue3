1d652a6d52c7f84fe35613e6cb8534c3763b7b1f
kimg0130 <kimg0130@gmail.com>
2023-04-24 21:27:25 +0900

    swiperjs test

--- src/views/swiper/SwiperTest.vue
+++ src/views/swiper/SwiperTest.vue
@@ -4,20 +4,67 @@ npm i swiper
 
   <v-divider class="mt-5 mb-5"></v-divider>
 
+  <div class="font-weight-bold text-blue">기본</div>
   <swiper
     :slides-per-view="1"
     :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
   >
     <swiper-slide v-for="(item,idx) in prdList.list" :key="idx"><img :src="item.thumbnail" style="width: 100%; height: 200px"></swiper-slide>
   </swiper>
+
+  <v-divider class="mt-5 mb-5"></v-divider>
+
+  <div class="font-weight-bold text-blue">navigation</div>
+  <swiper
+    :slides-per-view="1"
+    :space-between="50"
+    navigation
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+    :modules="[Navigation]"
+  >
+    <swiper-slide v-for="(item,idx) in prdList.list" :key="idx"><img :src="item.thumbnail" style="width: 100%; height: 200px"></swiper-slide>
+  </swiper>
+
+  <v-divider class="mt-5 mb-5"></v-divider>
+
+  <div class="font-weight-bold text-blue">Pagination</div>
+  <swiper
+    :slides-per-view="1"
+    :space-between="50"
+    :pagination="{ clickable: true }"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+    :modules="[Pagination]"
+  >
+    <swiper-slide v-for="(item,idx) in prdList.list" :key="idx"><img :src="item.thumbnail" style="width: 100%; height: 200px"></swiper-slide>
+  </swiper>
+
+
+  <v-divider class="mt-5 mb-5"></v-divider>
+
+  <div class="font-weight-bold text-blue">Scrollbar</div>
+  <swiper
+    :slides-per-view="1"
+    :space-between="50"
+    :scrollbar="{ draggable: true }"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+    :modules="[Scrollbar]"
+  >
+    <swiper-slide v-for="(item,idx) in prdList.list" :key="idx"><img :src="item.thumbnail" style="width: 100%; height: 200px"></swiper-slide>
+  </swiper>
+
 </template>
 
 <script setup>
 import {Swiper,SwiperSlide} from "swiper/vue";
+import {Navigation,Pagination,Scrollbar} from "swiper";
 import {getCurrentInstance, onMounted, reactive} from "vue";
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const { proxy } = getCurrentInstance();
 let prdList = reactive({
@@ -33,7 +80,7 @@ const getList = async () =>{
   let res = await proxy.$axios.get('/products')
   console.log(res.data)
   prdList.list = []
-  prdList.list.push(...res.data.products);
+  prdList.list.push(...res.data.products.slice(0,5));
 }
 
 const onSwiper = (swiper) => {
